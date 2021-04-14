e250225fc08955a58253b68b7a0d14a19e6b1a99
M-Picco <mpicco@fi.uba.ar>
2021-04-13 23:10:26 -0300

    Initial location retrieval

--- screens/Map.js
+++ screens/Map.js
@@ -1,15 +1,56 @@
 import React from 'react';
-import { StyleSheet, View , Dimensions} from 'react-native';
+import { Alert, StyleSheet, View , Dimensions, Text} from 'react-native';
 import  MapView from 'react-native-maps';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import * as Location from 'expo-location';
 
-const current_location={coords:{latitude:-34.61745 , longitude:-58.36795}};
-
+const DEFAULT_LOCATION = {
+  coords: {
+    latitude:-34.61745,
+    longitude:-58.36795
+  }
+};
+const DEFAULT_DELTA = 0.045;
 
 export default class  Map extends React.Component {
+  state = {
+    location:DEFAULT_LOCATION
+  }
 
-  state={
-    location:current_location,
+  componentDidMount() {
+    this.getCurrentLocation();
+  }
+
+  async getCurrentLocation() {
+    let enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled){
+      Alert.alert(
+        'Enable your location service.',
+        'This app requires location services.',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+      return;
+    }
+
+    let { status } = await Location.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert(
+        'Permission not granted',
+        'Allow the app to use location service.',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High
+    });
+
+    this.setState({ location })
   }
 
   render(){
@@ -19,12 +60,12 @@ export default class  Map extends React.Component {
           <MapView 
             style={styles.mapStyle}  
             ref={map => this._map = map}
-            initialRegion={{                      
-                            latitude: this.state.location.coords.latitude,
-                            longitude: this.state.location.coords.longitude,
-                            latitudeDelta: 0.0922,
-                            longitudeDelta: 0.0921,
-                          }} 
+            region={{
+              latitude: this.state.location.coords.latitude,
+              longitude: this.state.location.coords.longitude,
+              latitudeDelta: DEFAULT_DELTA,
+              longitudeDelta: DEFAULT_DELTA
+            }}
           > 
           </MapView>
         </View>
@@ -34,11 +75,11 @@ export default class  Map extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    mapStyle: {
-     width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
-    containerAll:{
-      ...StyleSheet.absoluteFillObject
-    },  
-  });
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  containerAll:{
+    ...StyleSheet.absoluteFillObject
+  }
+});

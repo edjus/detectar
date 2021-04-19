ca9c5fc0094a9049c4b71f0bcea62d349ad757a5
M-Picco <mpicco@fi.uba.ar>
2021-04-19 14:36:04 -0300

    Initial check for network connection

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import InternetConnectionAlert from "react-native-internet-connection-alert";
 import Texts from './utils/Texts';
 import Toast from 'react-native-toast-message';
-
+import * as Network from 'expo-network';
 
 export default function App() {
+  const [appState, setAppState] = useState({
+    connection: {
+      firstCheck: false,
+      active: false
+    }
+  })
+
+  if (!appState.connection.firstCheck) {
+    Network.getNetworkStateAsync().then(state => {
+      setAppState({
+        connection: {
+          active: state.isInternetReachable,
+          firstCheck: true
+        }
+      })
+    })
+
+    return (<View></View>)
+  }
+
+  // Network is fetched asynchronically, which means it is not
+  // immediately available on first render
+  if (appState.connection.firstCheck && !appState.connection.active) {
+    return (
+      <View>
+        {Alert.alert(
+          Texts.internetProblemTitle(),
+          Texts.internetProblemMessage(),
+          [{ text: 'OK' }],
+          { cancelable: false }
+        )}
+      </View>
+    )
+  }
+
   return (
     <InternetConnectionAlert
       title={Texts.internetProblemTitle()}
